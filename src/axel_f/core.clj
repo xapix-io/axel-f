f0775dcc3e486e390c5a2821f6f1b3ae77a8e702
Anatoly Smolyaninov <zarkone@ya.ru>
2018-07-26 11:32:20 +0200

    Add IF, bool operations to grammar

--- src/axel_f/core.clj
+++ src/axel_f/core.clj
@@ -15,29 +15,36 @@
 (def whitespace-symbols
   #{"\\s"})
 
-(def operators #{"+" "-" "/" "*"})
+(def operators #{"+" "-" "/" "*"
+                 "=" "!=" "<" ">" "<=" ">="})
+
+(def functions #{"IF" "SUM" "MIN"})
 
 (def grammar
   (str
    "
-   EXPR = <paren_op> EXPR <paren_close> | FNCALL | number | EXPR <whitespace> OPERATOR <whitespace> EXPR | REF
+   EXPR = <paren_op> EXPR <paren_close> | FNCALL | MULT_EXPR | number |  REF
+   MULT_EXPR = EXPR | MULT_EXPR <whitespace>* OPERATOR <whitespace>* EXPR
    FNCALL = FNNAME <paren_op> ARGLIST <paren_close>
-   FNNAME = wordnumber
-   ARGLIST = ARG | ARG <comma> ARGLIST
+   FNNAME = " (set->rule functions) "
+   ARGLIST = ARG | ARG <whitespace>* <comma> <whitespace>* ARGLIST
    <ARG> = EXPR
    OPERATOR = " (set->rule operators) "
    REF = OBJ <dot> FIELD | REF <dot> FIELD
    OBJ = wordnumber
    FIELD = wordnumber
+   <if> = 'if'
    <comma> = ','
    <dot> = '.'
    <paren_op> = '('
    <paren_close> = ')'
    <whitespace> = " (set->regex whitespace-symbols) "
-   <wordnumber> = #'[a-zA-Z0-9_]+'
-   <number> = #'[0-9]+'
+   wordnumber = #'[a-zA-Z0-9_]+'
+   number = #'[0-9]+'
 
   "))
+
+
 (def parser
   (insta/parser grammar :output-format :enlive))
 
