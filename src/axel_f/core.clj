56be948bc4eb9ac87c7f9711391d7988cdb97cf2
Kirill Chernyshov <delaguardo@gmail.com>
2018-08-10 16:04:08 +0200

    Change BNF for handling operator precedens
    Add transform functionality for calculating result on the fly

--- src/axel_f/core.clj
+++ src/axel_f/core.clj
@@ -1,156 +1,169 @@
 (ns axel-f.core
-  (:require [instaparse.core :as insta]
-            [clojure.string :as string]))
-
-(defn- set->regex [string-set]
-  (str "#'("
-       (string/join "|" string-set)
-       ")+'"))
-
-(defn- set->rule [string-set]
-  (->> string-set
-     (map #(str "'" % "'"))
-     (string/join " | ")))
-
-(def whitespace-symbols
-  #{"\\s"})
-
-(def functions #{"IF" "SUM" "MIN"})
-
-(def grammar
-  (str
-   "
-   EXPR = <paren_op> EXPR <paren_close> | FNCALL | ADD_EXPR | number | OBJREF
-   ADD_EXPR = MULT_EXPR | ADD_EXPR <whitespace>* ('+' | '-' ) <whitespace>* MULT_EXPR
-   MULT_EXPR = number | MULT_EXPR <whitespace>* ('*' | '/') <whitespace>* EXPR
-   FNCALL = FNNAME <paren_op> ARGLIST <paren_close>
-   FNNAME = " (set->rule functions) "
-   ARGLIST = ARG | ARG <whitespace>* <comma> <whitespace>* ARGLIST
-   <ARG> = EXPR
-   OBJREF = REF
-   REF = OBJ | REF <dot> FIELD
-   OBJ = identifier
-   FIELD = identifier
-   <if> = 'if'
-   <comma> = ','
-   <dot> = '.'
-   <paren_op> = '('
-   <paren_close> = ')'
-   <whitespace> = " (set->regex whitespace-symbols) "
-   identifier = #'[a-zA-Z_]+[a-zA-Z0-9_]*'
-   number = #'-?[0-9]+'
-
-  "))
-
+  (:require [instaparse.core :as insta]))
 
 (def parser
-  (insta/parser grammar))
+  (insta/parser
+   (str
+    "
+FORMULA ::= EXPR | <eq-op> EXPR | <formula-begining> EXPR <formula-ending>
+EXPR ::= MORE_EXPR | LESS_EXPR | MORE_OR_EQ_EXPR | LESS_OR_EQ_EXPR | EQ_EXPR | NOT_EQ_EXPR
+MORE_EXPR ::= CONCAT_EXPR {<more-op> CONCAT_EXPR}
+LESS_EXPR ::= CONCAT_EXPR {<less-op> CONCAT_EXPR}
+MORE_OR_EQ_EXPR ::= CONCAT_EXPR {<more-or-eq-op> CONCAT_EXPR}
+LESS_OR_EQ_EXPR ::= CONCAT_EXPR {<less-or-eq-op> CONCAT_EXPR}
+EQ_EXPR ::= CONCAT_EXPR {<eq-op> CONCAT_EXPR}
+NOT_EQ_EXPR ::= CONCAT_EXPR {<not-eq-op> CONCAT_EXPR}
+CONCAT_EXPR ::= ( ADD_EXPR | SUB_EXPR ) {<concat-op> ( ADD_EXPR | SUB_EXPR )}
+ADD_EXPR ::= ( MULT_EXPR | DIV_EXPR ) {<add-op> ( MULT_EXPR | DIV_EXPR )}
+SUB_EXPR ::= ( MULT_EXPR | DIV_EXPR ) {<sub-op> ( MULT_EXPR | DIV_EXPR )}
+MULT_EXPR ::= EXP_EXPR {<mult-op> ( EXP_EXPR | MULT_EXPR | DIV_EXPR )}
+DIV_EXPR ::= EXP_EXPR {<div-op> ( EXP_EXPR | MULT_EXPR | DIV_EXPR )}
+EXP_EXPR ::= PRIMARY {<exp-op> PRIMARY}
+PRIMARY ::= <whitespace> * ( <opening-parenthesis> EXPR <closing-parenthesis> | ( CONST / OBJREF ) | FNCALL | SIGN_EXPR | PERCENT_EXPR ) <whitespace> *
+SIGN_EXPR ::= ( '+' | '-' ) PRIMARY
+PERCENT_EXPR ::= PRIMARY <percent-operator>
+CONST ::= NUMBER | STRING | BOOL
+NUMBER ::= #'[0-9]+\\.?[0-9]*(e[0-9]+)?'
+STRING ::= #'\"[^\"]+\"'
+BOOL ::= #'TRUE|FALSE'
+FNCALL ::= FN <opening-parenthesis> ARGUMENTS <closing-parenthesis>
+FN ::= #'(SUM|SUMIF|IF|MIN|MAX|ROUND|LEN|CONCATENATE)'
+ARGUMENTS ::= ARGUMENT {<comma> ARGUMENT}
+ARGUMENT ::= EXPR | Epsilon
+OBJREF ::= FIELD {<dot> FIELD}
+FIELD ::= #'[a-zA-Z_]+[a-zA-Z0-9_]*'
+<formula-begining> ::= '{='
+<formula-ending> ::= '}'
+<whitespace> ::= #'(\\s)+'
+<concat-op> ::= '&'
+<exp-op> ::= '^'
+<percent-operator> ::= '%'
+<opening-parenthesis> ::= '('
+<closing-parenthesis> ::= ')'
+<more-op> ::= '>'
+<less-op> ::= '<'
+<more-or-eq-op> ::= '>='
+<less-or-eq-op> ::= '<='
+<eq-op> ::= '='
+<not-eq-op> ::= '<>'
+<add-op> ::= '+'
+<sub-op> ::= '-'
+<mult-op> ::= '*'
+<div-op> ::= '/'
+<comma> ::= ','
+<dot> ::= '.'
+  ")))
 
-;; (clojure.pprint/pprint (parser "SUM(foo.baz.bar - MIN(1,2) + 11)"))
+(defn transforms [context]
+  {:MORE_EXPR (fn
+                ([arg] arg)
+                ([arg & args]
+                 (apply > arg args)))
+   :MULT_EXPR (fn
+                ([arg] arg)
+                ([arg & args]
+                 (apply * arg args)))
+   :CONCAT_EXPR (fn
+                  ([arg] arg)
+                  ([arg & args]
+                   (apply str arg args)))
+   :BOOL (fn [b]
+           (case b
+             "TRUE" true
+             "True" true
+             "true" true
+             "FALSE" false
+             "False" false
+             "false" false))
+   :NOT_EQ_EXPR (fn
+                  ([arg] arg)
+                  ([arg & args]
+                   (apply not= arg args)))
+   :NUMBER read-string
+   :FNCALL (fn [f args]
+             (case f
+               "SUM" (apply +' args)
+               "IF" (if (first args)
+                      (second args)
+                      (nth args 2 nil))
+               "MIN" (apply min args)
+               "MAX" (apply max args)
+               "ROUND" (with-precision (second args) (first args))
+               "LEN" (count (flatten args))
+               "CONCATENATE" (apply str args)))
+   :SIGN_EXPR (fn [sign n]
+                (if (= sign "-")
+                  (* -1 n)
+                  n))
+   :LESS_OR_EQ_EXPR (fn
+                      ([arg] arg)
+                      ([arg & args]
+                       (apply <= arg args)))
+   :SUB_EXPR (fn
+               ([arg] arg)
+               ([arg & args]
+                (apply - arg args)))
+   :ADD_EXPR (fn
+               ([arg] arg)
+               ([arg & args]
+                (apply + arg args)))
+   :CONST identity
+   :FN identity
+   :EXP_EXPR (fn
+               ([arg] arg)
+               ([base power]
+                (Math/pow base power)))
+   :DIV_EXPR (fn
+               ([arg] arg)
+               ([arg & args]
+                (apply / arg args)))
+   :ARGUMENT identity
+   :STRING (fn [s]
+             (apply str
+                    (-> s
+                        rest
+                        butlast)))
+   :PRIMARY identity
+   :FIELD identity
+   :EQ_EXPR (fn
+              ([arg] arg)
+              ([arg & args]
+               (apply = arg args)))
+   :ARGUMENTS vector
+   :FORMULA identity
+   :LESS_EXPR (fn
+                ([arg] arg)
+                ([arg & args]
+                 (apply < arg args)))
+   :EXPR identity
+   :OBJREF (fn [& fields]
+             (get-in context fields))
+   :MORE_OR_EQ_EXPR (fn
+                      ([arg] arg)
+                      ([arg & args]
+                       (apply >= arg args)))})
 
-;; (parser "obj.")
-
-(def ^:dynamic *object-context* {})
-
-(defn operation [str-op & args]
-  (let [op (-> str-op symbol resolve)]
-    (apply op args)))
-
-(defmulti run #(first %))
-
-(defmethod run :EXPR EXPR [nodes]
-  (run (second nodes)))
-
-(defmethod run :number number [[_ number-str]]
-  (Integer/parseInt number-str))
-
-(defn math-expr [child-nodes]
-  (if (= (count child-nodes) 3)
-    (let [[expr1 str-op expr2] child-nodes]
-      (operation
-       str-op (run expr1) (run expr2)))
-    (run (first child-nodes))))
-
-(defmethod run :ADD_EXPR ADD_EXPR [[_ & child-nodes]]
-  (math-expr child-nodes))
-
-(defmethod run :MULT_EXPR MULT_EXPR [[_ & child-nodes]]
-  (math-expr child-nodes))
-
-(defmulti fncall #(first %))
-
-(defmethod fncall :MIN MIN [[_ arglist]]
-  (apply min arglist))
-
-(defmethod fncall :SUM SUM [[_ arglist]]
-  (apply + arglist))
-
-(defmethod run :FNCALL FNCALL [[_ & [[_ fn-name] arglist-node]]]
-  (fncall  [(keyword fn-name)
-            (flatten (run arglist-node))]))
-
-(defmethod run :ARGLIST ARGLIST [[node-name & arglist]]
-  (map run arglist))
-
-(defmethod run :OBJREF OBJREF [[node-name & arglist]]
-  (get-in *object-context*
-          (run (first arglist))))
-
-(defmethod run :REF REF [[node-name & arglist]]
-  (concat (run (first arglist))
-          (when-let [tail (-> arglist second second second)]
-            [tail])))
-
-(defmethod run :FIELD FIELD [[node-name & arglist]]
-  (-> arglist second second))
-
-(defmethod run :OBJ OBJ [[node-name & arglist]]
-  [(second (first arglist))])
-
+(defn run [formula context]
+  (insta/transform
+   (transforms context)
+   (parser formula)))
 
 (comment
-  (parser "abc.foo.bar.baz")
-  (parser "foo(1 + 1)")
-  (parser "1")
-  (run (parser "-1"))
-  (run (parser "1 + 2 - 3 + 4"))
-  (parser "1 + 2 - 3 + 4")
+  (time
+   (run "SUM(1,2,3) / LEN(foo.bar) * IF(foo.baz > foo.buz, -1, 1)"
+     {"foo" {"bar" [5 6 7]
+             "baz" 10
+             "buz" 9}}
+     ))
 
-  (binding [*object-context* {"abc" {"foo" {"bar" {"baz" 42}}}}]
-    (run
-      (parser "abc.foo.bar.baz * 2")
-      ))
+  (require '[criterium.core :as b])
 
-  (binding [*object-context* {"foo" {"baz" {"bar" 42}}}]
-    (run
-      (parser "SUM(foo.baz.bar, 8, 10) - MIN(1,2) + 11")
-
-      ))
-
-  (run (parser "2*3 + 2 - 3 * 4 - 1*2"))
-
-  (clojure.pprint/pprint
-   (parser "1 + 2 - 3 + 4"))
-
-
-  (clojure.pprint/pprint
-  (parser "MIN(1,2)"))
-
-  (run (parser "MIN(1,2,2,3,4,5,11,1,2,3,4)"))
-
-  (run (parser "SUM(10 - MIN(3,2) * 11)"))
-
-  (run (parser "SUM(1 + 1)"))
-
-  (parser "SUM(-(1 + 1)")
-
-  (clojure.pprint/pprint
-   (parser "1 + 1"))
-
-  (run
-    (parser "1+1-2"))
-
-  (run
-    (parser "11 > 2"))
+  (b/bench
+   (run "SUM(1,2,3) / LEN(foo.bar) * IF(foo.baz > foo.buz, -1, 1)"
+     {"foo" {"bar" [5 6 7]
+             "baz" 10
+             "buz" 9}}
+     )
+   )
 
   )
