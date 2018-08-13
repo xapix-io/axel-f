b9bfc4f7a54978a9e0dc68015b8ae787fea7bf60
Kirill Chernyshov <delaguardo@gmail.com>
2018-08-13 14:27:35 +0200

    Fix operator precedence and add interface functions

--- src/axel_f/core.clj
+++ src/axel_f/core.clj
@@ -1,45 +1,52 @@
 (ns axel-f.core
-  (:require [instaparse.core :as insta]))
+  (:require [instaparse.core :as insta])
+  (:refer-clojure :exclude [compile]))
 
 (def parser
   (insta/parser
-   (str
-    "
+   "
 FORMULA ::= EXPR | <eq-op> EXPR | <formula-begining> EXPR <formula-ending>
-EXPR ::= MORE_EXPR | LESS_EXPR | MORE_OR_EQ_EXPR | LESS_OR_EQ_EXPR | EQ_EXPR | NOT_EQ_EXPR
-MORE_EXPR ::= CONCAT_EXPR {<more-op> CONCAT_EXPR}
-LESS_EXPR ::= CONCAT_EXPR {<less-op> CONCAT_EXPR}
-MORE_OR_EQ_EXPR ::= CONCAT_EXPR {<more-or-eq-op> CONCAT_EXPR}
-LESS_OR_EQ_EXPR ::= CONCAT_EXPR {<less-or-eq-op> CONCAT_EXPR}
-EQ_EXPR ::= CONCAT_EXPR {<eq-op> CONCAT_EXPR}
-NOT_EQ_EXPR ::= CONCAT_EXPR {<not-eq-op> CONCAT_EXPR}
-CONCAT_EXPR ::= ( ADD_EXPR | SUB_EXPR ) {<concat-op> ( ADD_EXPR | SUB_EXPR )}
-ADD_EXPR ::= ( MULT_EXPR | DIV_EXPR ) {<add-op> ( MULT_EXPR | DIV_EXPR )}
-SUB_EXPR ::= ( MULT_EXPR | DIV_EXPR ) {<sub-op> ( MULT_EXPR | DIV_EXPR )}
-MULT_EXPR ::= EXP_EXPR {<mult-op> ( EXP_EXPR | MULT_EXPR | DIV_EXPR )}
-DIV_EXPR ::= EXP_EXPR {<div-op> ( EXP_EXPR | MULT_EXPR | DIV_EXPR )}
+EXPR ::= COMPARISON_EXPS
+COMPARISON_EXPS ::= MORE_EXPR | LESS_EXPR | MORE_OR_EQ_EXPR | LESS_OR_EQ_EXPR | EQ_EXPR | NOT_EQ_EXPR
+MORE_EXPR ::= ( CONCAT_EXPR | COMPARISON_EXPS ) {<more-op> COMPARISON_EXPS }
+LESS_EXPR ::= ( CONCAT_EXPR | COMPARISON_EXPS ) {<less-op> COMPARISON_EXPS }
+MORE_OR_EQ_EXPR ::= ( CONCAT_EXPR | COMPARISON_EXPS ) {<more-or-eq-op> COMPARISON_EXPS }
+LESS_OR_EQ_EXPR ::= ( CONCAT_EXPR | COMPARISON_EXPS ) {<less-or-eq-op> COMPARISON_EXPS }
+EQ_EXPR ::= ( CONCAT_EXPR | COMPARISON_EXPS ) {<eq-op> COMPARISON_EXPS }
+NOT_EQ_EXPR ::= ( CONCAT_EXPR | COMPARISON_EXPS ) {<not-eq-op> COMPARISON_EXPS }
+CONCAT_EXPR ::= ADDITIVE_EXPS {<concat-op> ADDITIVE_EXPS}
+ADDITIVE_EXPS ::= ADD_EXPR | SUB_EXPR
+ADD_EXPR ::= ( MULTIPLICATIVE_EXPS | ADDITIVE_EXPS ) {<add-op> MULTIPLICATIVE_EXPS}
+SUB_EXPR ::= ( MULTIPLICATIVE_EXPS | ADDITIVE_EXPS ) {<sub-op> MULTIPLICATIVE_EXPS}
+MULTIPLICATIVE_EXPS ::= MULT_EXPR | DIV_EXPR
+MULT_EXPR ::= ( EXP_EXPR | MULTIPLICATIVE_EXPS ) {<mult-op> EXP_EXPR}
+DIV_EXPR ::= ( EXP_EXPR | MULTIPLICATIVE_EXPS ) {<div-op> EXP_EXPR}
 EXP_EXPR ::= PRIMARY {<exp-op> PRIMARY}
 PRIMARY ::= <whitespace> * ( <opening-parenthesis> EXPR <closing-parenthesis> | ( CONST / OBJREF ) | FNCALL | SIGN_EXPR | PERCENT_EXPR ) <whitespace> *
 SIGN_EXPR ::= ( '+' | '-' ) PRIMARY
-PERCENT_EXPR ::= PRIMARY <percent-operator>
+PERCENT_EXPR ::= PRIMARY <percent-op>
 CONST ::= NUMBER | STRING | BOOL
 NUMBER ::= #'[0-9]+\\.?[0-9]*(e[0-9]+)?'
 STRING ::= #'\"[^\"]+\"'
-BOOL ::= #'TRUE|FALSE'
+BOOL ::= #'TRUE|FALSE|True|False|true|false'
 FNCALL ::= FN <opening-parenthesis> ARGUMENTS <closing-parenthesis>
-FN ::= #'(SUM|SUMIF|IF|MIN|MAX|ROUND|LEN|CONCATENATE)'
+FN ::= #'(SUM|IF|MIN|MAX|ROUND|LEN|CONCATENATE|AVERAGE)'
 ARGUMENTS ::= ARGUMENT {<comma> ARGUMENT}
 ARGUMENT ::= EXPR | Epsilon
-OBJREF ::= FIELD {<dot> FIELD}
-FIELD ::= #'[a-zA-Z_]+[a-zA-Z0-9_]*'
+OBJREF ::= FIELD (<dot> FIELD)+
+FIELD ::= #'(\"[^\"]+\"|[a-zA-Z0-9-_]+)(\\[[0-9]+\\])?'
+STRING_FIELD ::= STRING
+SYMBOL_FIELD ::= #'[a-zA-Z0-9-_]+'
+<opening-square-bracket> ::= '['
+<closing-square-bracket> ::= ']'
 <formula-begining> ::= '{='
 <formula-ending> ::= '}'
 <whitespace> ::= #'(\\s)+'
-<concat-op> ::= '&'
-<exp-op> ::= '^'
-<percent-operator> ::= '%'
 <opening-parenthesis> ::= '('
 <closing-parenthesis> ::= ')'
+<percent-op> ::= '%'
+<concat-op> ::= '&'
+<exp-op> ::= '^'
 <more-op> ::= '>'
 <less-op> ::= '<'
 <more-or-eq-op> ::= '>='
@@ -52,118 +59,174 @@ FIELD ::= #'[a-zA-Z_]+[a-zA-Z0-9_]*'
 <div-op> ::= '/'
 <comma> ::= ','
 <dot> ::= '.'
-  ")))
+  "))
 
-(defn transforms [context]
-  {:MORE_EXPR (fn
-                ([arg] arg)
-                ([arg & args]
-                 (apply > arg args)))
-   :MULT_EXPR (fn
-                ([arg] arg)
-                ([arg & args]
-                 (apply * arg args)))
-   :CONCAT_EXPR (fn
-                  ([arg] arg)
-                  ([arg & args]
-                   (apply str arg args)))
-   :BOOL (fn [b]
-           (case b
-             "TRUE" true
-             "True" true
-             "true" true
-             "FALSE" false
-             "False" false
-             "false" false))
-   :NOT_EQ_EXPR (fn
-                  ([arg] arg)
-                  ([arg & args]
-                   (apply not= arg args)))
-   :NUMBER read-string
-   :FNCALL (fn [f args]
-             (case f
-               "SUM" (apply +' args)
-               "IF" (if (first args)
-                      (second args)
-                      (nth args 2 nil))
-               "MIN" (apply min args)
-               "MAX" (apply max args)
-               "ROUND" (with-precision (second args) (first args))
-               "LEN" (count (flatten args))
-               "CONCATENATE" (apply str args)))
-   :SIGN_EXPR (fn [sign n]
-                (if (= sign "-")
-                  (* -1 n)
-                  n))
-   :LESS_OR_EQ_EXPR (fn
-                      ([arg] arg)
-                      ([arg & args]
-                       (apply <= arg args)))
-   :SUB_EXPR (fn
-               ([arg] arg)
-               ([arg & args]
-                (apply - arg args)))
-   :ADD_EXPR (fn
-               ([arg] arg)
-               ([arg & args]
-                (apply + arg args)))
-   :CONST identity
-   :FN identity
-   :EXP_EXPR (fn
-               ([arg] arg)
-               ([base power]
-                (Math/pow base power)))
-   :DIV_EXPR (fn
-               ([arg] arg)
-               ([arg & args]
-                (apply / arg args)))
-   :ARGUMENT identity
-   :STRING (fn [s]
-             (apply str
-                    (-> s
-                        rest
-                        butlast)))
-   :PRIMARY identity
-   :FIELD identity
-   :EQ_EXPR (fn
-              ([arg] arg)
-              ([arg & args]
-               (apply = arg args)))
-   :ARGUMENTS vector
-   :FORMULA identity
-   :LESS_EXPR (fn
-                ([arg] arg)
-                ([arg & args]
-                 (apply < arg args)))
-   :EXPR identity
-   :OBJREF (fn [& fields]
-             (get-in context fields))
-   :MORE_OR_EQ_EXPR (fn
-                      ([arg] arg)
-                      ([arg & args]
-                       (apply >= arg args)))})
+(defn round2
+  "Round a double to the given precision (number of significant digits)"
+  [d precision]
+  (let [factor (Math/pow 10 precision)
+        res (/ (Math/round (* d factor)) factor)]
+    (if (> precision 0)
+      res
+      (int res))))
 
-(defn run [formula context]
+(defn optimize-token [token]
+  (fn
+    ([arg] arg)
+    ([arg & args]
+     (vec
+      (cons token
+            (cons arg args))))))
+
+(def optimize-transforms
+  {:EXPR                identity
+   :ARGUMENTS           vector
+   :ARGUMENT            identity
+   :FORMULA             identity
+   :PRIMARY             identity
+   :CONST               identity
+   :NUMBER              read-string
+   :FN                  identity
+   :FIELD               identity
+   :STRING              (fn [s] (apply str (-> s rest butlast)))
+   :BOOL                (fn [b]
+                          (case b
+                            "TRUE"  true
+                            "True"  true
+                            "true"  true
+                            "FALSE" false
+                            "False" false
+                            "false" false))
+   :COMPARISON_EXPS     identity
+   :ADDITIVE_EXPS       identity
+   :MULTIPLICATIVE_EXPS identity
+   :MORE_EXPR           (optimize-token :MORE_EXPR)
+   :MULT_EXPR           (optimize-token :MULT_EXPR)
+   :CONCAT_EXPR         (optimize-token :CONCAT_EXPR)
+   :NOT_EQ_EXPR         (optimize-token :NOT_EQ_EXPR)
+   :LESS_OR_EQ_EXPR     (optimize-token :LESS_OR_EQ_EXPR)
+   :SUB_EXPR            (optimize-token :SUB_EXPR)
+   :ADD_EXPR            (optimize-token :ADD_EXPR)
+   :EXP_EXPR            (optimize-token :EXP_EXPR)
+   :DIV_EXPR            (optimize-token :DIV_EXPR)
+   :EQ_EXPR             (optimize-token :EQ_EXPR)
+   :LESS_EXPR           (optimize-token :LESS_EXPR)
+   :MORE_OR_EQ_EXPR     (optimize-token :MORE_OR_EQ_EXPR)})
+
+(defn compile [formula-str]
   (insta/transform
-   (transforms context)
-   (parser formula)))
+   optimize-transforms
+   (parser formula-str)))
+
+(declare run*)
+
+(defn run-fncall* [f args context]
+  (case f
+    "SUM"         (reduce +' (flatten (map #(run* % context) args)))
+    "LEN"         (count (flatten (map #(run* % context) args)))
+    "MIN"         (reduce min (flatten (map #(run* % context) args)))
+    "MAX"         (reduce max (flatten (map #(run* % context) args)))
+    "CONCATENATE" (reduce str (flatten (map #(run* % context) args)))
+    "IF"          (if (run* (first args) context)
+                    (run* (second args) context)
+                    (when-let [else (nth args 2 nil)]
+                      (run* else context)))
+    "AVERAGE"     (if-let [l (not-empty (flatten (map #(run* % context) args)))]
+                    (/ (reduce +' l)
+                       (count l)))
+    "ROUND"       (let [d (double (run* (first args) context))
+                        p (second args)
+                        p (if p (run* p context) 0)]
+                    (round2 d p))))
+
+(defn run* [arg context]
+  (let [token (if (vector? arg)
+                (first arg)
+                arg)
+        args  (if (vector? arg)
+                (rest arg)
+                nil)]
+    (cond
+      (or (string? token)
+          (number? token)
+          (boolean? token))
+      token
+
+      (keyword? token)
+      (case token
+        :MORE_EXPR       (apply >
+                                (map #(run* % context) args))
+        :MORE_OR_EQ_EXPR (apply >=
+                                (map #(run* % context) args))
+        :LESS_EXPR       (apply <
+                                (map #(run* % context) args))
+        :LESS_OR_EQ_EXPR (apply <=
+                                (map #(run* % context) args))
+        :EQ_EXPR         (apply =
+                                (map #(run* % context) args))
+        :NOT_EQ_EXPR     (apply not=
+                                (map #(run* % context) args))
+        :MULT_EXPR       (apply *'
+                                (map #(run* % context) args))
+        :DIV_EXPR        (apply /
+                                (map #(run* % context) args))
+        :CONCAT_EXPR     (apply str
+                                (map #(run* % context) args))
+        :SUB_EXPR        (apply -'
+                                (map #(run* % context) args))
+        :ADD_EXPR        (apply +'
+                                (map #(run* % context) args))
+        :SIGN_EXPR       (let [r (run* (second args) context)]
+                           (if (= (first args) "-")
+                             (* -1 r)
+                             r))
+        :OBJREF          (get-in context (map #(run* % context) args))
+        :FNCALL          (run-fncall* (first args) (second args) context)))))
+
+(defn run
+  ([formula] (run formula {}))
+  ([formula context]
+   (let [formula (if (string? formula)
+                   (compile formula)
+                   formula)]
+             (run* formula context))))
 
 (comment
+  (def context {"foo" {"bar" [0.37990796030362173
+                              0.18809681687624025
+                              1.09344349517165595
+                              0.013367883168072914
+                              0.15845551027572413
+                              3.46994775748326534
+                              0.059319921394827]
+                       "baz" 10
+                       "buz" 9}})
+
+  (run "-1 * 9 + 12 < 0" {})
+  (clojure.pprint/pprint
+   (time
+    (run [:FNCALL "ROUND" [123.321 [:SIGN_EXPR "-" 2]]]
+      {"foo" {"bar" -2
+              "baz" 9}}))
+   )
+
+  (run
+    ;; (compile "SUM(foo.bar) / LEN(foo.bar)")
+    "AVERAGE(foo.bar)"
+    context)
+
+  (run (compile "=9000") context)
+
   (time
-   (run "SUM(1,2,3) / LEN(foo.bar) * IF(foo.baz > foo.buz, -1, 1)"
-     {"foo" {"bar" [5 6 7]
-             "baz" 10
-             "buz" 9}}
-     ))
+   (run
+     "SUM(foo.bar, 9000) / LEN(foo.bar) * (foo.baz + 9)"
+     context)
+   )
 
-  (require '[criterium.core :as b])
-
-  (b/bench
-   (run "SUM(1,2,3) / LEN(foo.bar) * IF(foo.baz > foo.buz, -1, 1)"
-     {"foo" {"bar" [5 6 7]
-             "baz" 10
-             "buz" 9}}
-     )
+  (time
+   (run
+     "IF(foo.baz <> foo.buz, false, true)"
+     context)
    )
 
   )
