931c492157608daa6659366673c286688b958db3
Kirill Chernyshov <delaguardo@gmail.com>
2019-02-01 13:15:50 +0100

    Cleanup JS api namespace

--- src/axel_f/api.cljs
+++ src/axel_f/api.cljs
@@ -1,32 +1,10 @@
 (ns axel-f.api
   (:require [axel-f.core :as axel-f]))
 
-(defn- fix-regex-in-exception [exception-data]
-  (update exception-data
-          :reason
-          (fn [reasons]
-            (mapv (fn [{:keys [tag] :as reason}]
-                    (if (= tag :regexp)
-                      (update reason :expecting str)
-                      reason))
-                  reasons))))
-
 (defn ^:export compile [formula-str]
   (try
-    (clj->js (axel-f/compile formula-str))
+    (let [f (axel-f/compile formula-str)]
+      (fn [ctx]
+        (clj->js (f (js->clj ctx)))))
     (catch ExceptionInfo e
-      (throw (js/Error. (js/JSON.stringify (clj->js (fix-regex-in-exception
-                                                     (ex-data e)))))))))
-
-(defn ^:export run
-  ([formula] (run formula (clj->js {})))
-  ([formula context]
-   (let [context (js->clj context)]
-     (try
-       (let [formula (if (string? formula)
-                       formula
-                       (js->clj formula))]
-         (clj->js (axel-f/run formula context)))
-       (catch ExceptionInfo e
-         (throw (js/Error. (js/JSON.stringify (clj->js (fix-regex-in-exception
-                                                        (ex-data e)))))))))))
+      (throw (js/Error. (js/JSON.stringify (clj->js (ex-data e))))))))
--- src/axel_f/externs.js
+++ src/axel_f/externs.js
@@ -1,5 +1,4 @@
 var axel_f = {};
 axel_f.api = {};
 
-axel_f.api.run = function () {};
 axel_f.api.compile = function () {};
