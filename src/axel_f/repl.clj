6e452408139725c788e853617e78c13826ad9ac3
Kirill Chernyshov <delaguardo@gmail.com>
2019-04-04 17:57:03 +0200

    Small fixes for repl

--- src/axel_f/repl.clj
+++ src/axel_f/repl.clj
@@ -4,14 +4,22 @@
 (defn run
   "start read eval print loop"
   [ctx]
-  (println "press ctrl-D or type `end` to exit")
+  (println "press ctrl-D or type `exit` to exit")
   (loop []
     (print "? ")
     (flush)
     (let [formula (read-line)]
-      (if (= "end" formula)
-        (print "end")
-        (let [f (try (af/compile formula) (catch Throwable e (print "formula read error\n")))
-              res (when f (try (f ctx) (catch Throwable e (print "formula eval error\n"))))]
-          (when res (print "=" res "\n"))
+      (if (= "exit" formula)
+        (println "Buy!")
+        (let [f (try (af/compile formula)
+                     (catch Throwable e
+                       (println "formula read error\n")))
+              res (if f
+                    (try (f ctx)
+                         (catch Throwable e
+                           (println "formula eval error\n")
+                           ::fail))
+                    ::fail)]
+          (when-not (= ::fail res)
+            (println "=" res))
           (recur))))))
